9d1e9800a0da2d0c5e78e1ee8b2b8b8b24bdf1f2
te22E-Olsson-Anton <anton.olsson3@elev.ga.ntig.se>
2023-09-04 09:03:46 +0200

    kalkylator

---
+++ src/App.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class prprr {
+    
+
+    public static void main(String[] args)
+    {
+
+        Scanner tb = new Scanner (System.in);
+
+        System.out.print("Ange Tal: ");
+        int tal1 = tb.nextInt();
+        System.out.print("Ange Tal: ");
+        int tal2 = tb.nextInt();
+        
+
+        int summa = tal1 + tal2;
+
+        System.out.println(tal1+ "+" +tal2+ "=" +summa);
+
+
+
+
+    }
+
+
+}
